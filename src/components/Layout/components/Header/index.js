67ff121dd3470d8af3be518af362b5cb04b5adec
DT2301 <127176097+DT2301@users.noreply.github.com>
2024-07-08 17:21:45 +0700

    Fixed file issues and added placeholders

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,33 @@
 import classNames from 'classnames/bind';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles)
 
+console.log(images.logo);
 function Header() {
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
-            
+            <img src={images.logo} alt='TikTok' />
+
+            <div className={cx('search')}>
+                <input placeholder='Search accounts and videos' spellCheck={false} />
+                <button className={cx('clear')}>
+                    { /*clear*/}
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                { /*loadding*/}
+
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    { /*Search*/}
+                </button>
+            </div>
+            <div className={cx('action')}></div>
         </div>
     </header>
 }
